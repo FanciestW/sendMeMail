0eaed2721b77c1dab2a6d009aace48d1f46ffc4f
William Lin <wlin26@yahoo.com>
2019-07-30 21:33:20 -0400

    Added sending of test mail

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 const sgMail = require('@sendgrid/mail');
-require('dotenv').config();
 sgMail.setApiKey(process.env.SENDGRID_KEY);
+console.log(process.env.SENDGRID_KEY);
 
 export const email: APIGatewayProxyHandler = async (event, _context) => {
 	const body = JSON.parse(event.body);
 	const message = body.message || '';
 	const senderEmail = body.email || '';
 	console.log(`${senderEmail}\n${message}`);
+	const email = {
+		to: 'wlin26@yahoo.com',
+		from: senderEmail,
+		subject: `SendMeMail`,
+		text: message
+	};
+	sgMail.send(email);
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
