dd5e28297abab97cd01194a91929acb9439c8bbe
William Lin <wlin26@yahoo.com>
2019-07-30 22:10:12 -0400

    Removed debugging console.logs

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,11 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_KEY);
-console.log(process.env.SENDGRID_KEY);
 
 export const email: APIGatewayProxyHandler = async (event, _context) => {
 	const body = JSON.parse(event.body);
 	const message = body.message || '';
 	const senderEmail = body.email || '';
-	console.log(`${senderEmail}\n${message}`);
 	const email = {
 		to: 'wlin26@yahoo.com',
 		from: senderEmail,
