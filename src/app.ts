b4bb33c6d085b503bfa7393bef5650a48344ee86
William Lin <wlin26@yahoo.com>
2019-07-30 22:11:39 -0400

    Updated sendGrid sending with callback for error handling

--- src/app.ts
+++ src/app.ts
@@ -13,11 +13,26 @@ export const email: APIGatewayProxyHandler = async (event, _context) => {
 		subject: `SendMeMail`,
 		text: message
 	};
-	sgMail.send(email);
+	sgMail.send(email).then((_result: any) => {
+		return {
+			statusCode: 200,
+			body: JSON.stringify({
+				message: 'Mail Sent!'
+			}, null, 2),
+		};
+	}, (err: any) => {
+		return {
+			statusCode: 500,
+			body: JSON.stringify({
+				message: 'Message Failed to Send',
+				error: err,
+			})
+		};
+	});
 	return {
-		statusCode: 200,
+		statusCode: 500,
 		body: JSON.stringify({
-			message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!'
+			message: 'Error, mail never sent?!'
 		}, null, 2),
 	};
 }
