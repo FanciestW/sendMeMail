1c18206cdf388193a752e2176fe7f0b190a66577
William Lin <wlin26@yahoo.com>
2019-08-11 20:54:19 -0400

    Fixed tslint errors

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,32 @@
+const sgMail = require('@sendgrid/mail');
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
-const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_KEY);
 
 export const email: APIGatewayProxyHandler = (event, _context) => {
-	const body = JSON.parse(event.body);
-	const message = body.message || '';
-	const senderEmail = body.email || '';
-	const email = {
-		to: 'wlin26@yahoo.com',
-		from: senderEmail,
-		subject: `SendMeMail`,
-		text: message
-	};
-	sgMail.send(email).then((_result: any) => {
-		return {
-			statusCode: 200,
-			body: JSON.stringify({
-				message: 'Mail Sent!'
-			}, null, 2),
-		};
-	}, (err: any) => {
-		return {
-			statusCode: 500,
-			body: JSON.stringify({
-				message: 'Message Failed to Send',
-				error: err,
-			})
-		};
-	});
-}
+    const body = JSON.parse(event.body);
+    const message = body.message || '';
+    const senderEmail = body.email || '';
+    const emailData = {
+        to: 'wlin26@yahoo.com',
+        from: senderEmail,
+        subject: `SendMeMail`,
+        text: message,
+    };
+    sgMail.send(emailData).then((_result: any) => {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Mail Sent!',
+            }, null, 2),
+        };
+    }, (err: any) => {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'Message Failed to Send',
+                error: err,
+            }),
+        };
+    });
+};
