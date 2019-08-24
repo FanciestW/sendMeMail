d9be95d8986efcb664db4015b0bd6e96609557bd
William Lin <wlin26@yahoo.com>
2019-08-24 00:17:14 -0400

    Updated error message

--- src/app.ts
+++ src/app.ts
@@ -4,29 +4,60 @@ import 'source-map-support/register';
 sgMail.setApiKey(process.env.SENDGRID_KEY);
 
 export const email: APIGatewayProxyHandler = async (event, _context) => {
-    const body = JSON.parse(event.body);
-    const message = body.message || '';
-    const senderEmail = body.email || '';
-    const emailData = {
-        to: 'wlin26@yahoo.com',
-        from: senderEmail,
-        subject: `SendMeMail`,
-        text: message,
-    };
-    return sgMail.send(emailData).then((_result: any) => {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'Mail Sent!',
-            }, null, 2),
+    try {
+        const header = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
         };
-    }, (err: any) => {
+        const body = JSON.parse(event.body);
+        const message = body.message || '';
+        const senderEmail = body.email || '';
+        const emailData = {
+            to: 'wlin26@yahoo.com',
+            from: senderEmail,
+            subject: `SendMeMail`,
+            text: message,
+        };
+        return sgMail.send(emailData).then((_result: any) => {
+            return {
+                statusCode: 200,
+                headers: header,
+                body: JSON.stringify({
+                    message: 'Mail Sent!',
+                }, null, 2),
+            };
+        }, (err: any) => {
+            return {
+                statusCode: 500,
+                headers: header,
+                body: JSON.stringify({
+                    message: 'Message Failed to Send',
+                    error: err,
+                }),
+            };
+        });
+    } catch (err) {
+        const header = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        };
+        if (err instanceof TypeError) {
+            return {
+                statusCode: 400,
+                headers: header,
+                body: JSON.stringify({
+                    message: 'Request caused TypeError',
+                    error: err,
+                }),
+            };
+        }
         return {
             statusCode: 500,
+            headers: header,
             body: JSON.stringify({
-                message: 'Message Failed to Send',
+                message: 'Error Occurred',
                 error: err,
             }),
         };
-    });
+    }
 };
